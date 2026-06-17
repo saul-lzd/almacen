7a306dbe3686ead963a0379a7e18660073811da3
saul-lzd <46232187+saul-lzd@users.noreply.github.com>
2026-06-16 20:17:02 -0600

    BACKEND ::: Implement Swagger

--- almacen-backend/src/main/java/com/sesesp/almacen/config/SecurityConfig.java
+++ almacen-backend/src/main/java/com/sesesp/almacen/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
